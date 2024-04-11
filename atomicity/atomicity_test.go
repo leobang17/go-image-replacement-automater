7a8f8ea7aa37d49abf834bc7d41d6117924b3c3b
LeoBang <bluke8489@gmail.com>
2024-04-11 17:45:15 +0900

    test code for AtomicSection > only successive case

---
+++ atomicity/atomicity_test.go
@@ -0,0 +1,38 @@
+package atomicity
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func Test_AtomicSection_Success(t *testing.T) {
+	randomInts := []int {}
+	for i := 0; i < 20; i ++ {
+		randomInts = append(randomInts, 100_000 + rand.Intn(1_000_000))
+	}
+	t.Log(randomInts)
+
+	for _, randNum := range randomInts {
+		t.Run("",func(t *testing.T) {
+			t.Parallel()
+			atomicSection := NewAtomicSection[int]()
+			N := randNum
+			result, err := atomicSection(func (runnable Runnable[int]) {
+				t.Log(N)
+				for j := 0; j < N; j ++ {
+					runnable(func () (int, error) {
+						return j, nil
+					})
+				}
+			})
+
+			if err != nil {
+				t.Errorf("expected no error, but got %v", err)
+			}
+			if len(result) != N {
+				t.Errorf("expected %v length of array, but got %v length", N, len(result))
+			}
+			t.Logf("Actual Length: %v, Expected Length: %v", len(result), N)
+		})
+	}
+}
